a791349e1d28b1bceee0ac106ad995006d0e937e
JosueLC <josueflopez@msn.com>
2021-12-29 19:26:56 -0500

    creating backend folder structure

--- main.py
+++
@@ -1,9 +0,0 @@
-from scrapper.scrapper import parse_home, parse_course, parse_class
-from scrapper.scrapper import list_of_links, links_to_audios, texts_to_classes
-
-def run():
-    parse_home()
-
-if __name__ == '__main__':
-    run()
-    print('Finished.')
